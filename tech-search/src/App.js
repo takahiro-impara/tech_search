9490e8b7971678ccd4317b536d83f35c9fc23537
impara8 <takahiro4120@gmail.com>
2022-11-27 22:45:03 +0900

    add frontend

--- tech-search/src/App.js
+++ tech-search/src/App.js
@@ -3,7 +3,7 @@ import Card from './components/Card';
 
 import { useState, useEffect } from 'react';
 
-const endpoint = "http://backend.sandbox.us-east-2.tech-search:8080/techsearch/v1/blogs"
+const endpoint = "http://teck-search-backend.staging.udacity.impara8.com:8080/techsearch/v1/blogs"
 
 function App() {
   const [posts, setPosts] = useState([]);
