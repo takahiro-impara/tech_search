d98c77ddf546e2f78e1aba04fe848ab94a069946
impara8 <takahiro4120@gmail.com>
2022-11-27 21:47:01 +0900

    add manifest

---
+++ shell/createsv.sh
@@ -0,0 +1,1 @@
+eksctl create iamserviceaccount --name external-dns --namespace tech-search-prod --cluster education-eks-gTzzcO0P --region us-east-2 --attach-policy-arn arn:aws:iam::882275506731:policy/ud-handson-ExternalDNS --approve
