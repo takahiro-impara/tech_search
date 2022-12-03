6accc9b0369ab35fecc6ca1ce91043bc354dacad
impara8 <takahiro4120@gmail.com>
2022-12-03 22:59:44 +0900

    add ui

--- tech-search/src/App.js
+++ tech-search/src/App.js
@@ -1,36 +1,47 @@
 import './App.css';
-import Card from './components/Card';
+import CardProps from './components/Card';
+import Loading from './components/Loading';
 
 import { useState, useEffect } from 'react';
 
 const SEARCH_ENDPOINT = process.env.REACT_APP_SEARCH_ENDPOINT
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [posts, setPosts] = useState([]);
   useEffect(() => {
+      setIsLoading(true);
       fetch(SEARCH_ENDPOINT)
         .then((res) => res.json())
         .then((data) => {
             setPosts(data);
+            setIsLoading(false);
         })
         .catch((err) => {
             console.log(err.message);
+            setIsLoading(false);
         });
   }, []);
   return (
     <div className="App">
       <header className="App-header">
           <div className="contents">
+            { isLoading ? (
+              <div className="blog-container">
+                <Loading />
+              </div>
+            ) : (
             <div className="blog-container">
               {posts.map((blog, index) => 
-                  <Card 
-                    key={index}
-                    Title = {blog.Title}
-                    Date = {blog.Date}
-                    Url = {blog.Url}
-                    Company = {blog.Company}
-                  />
+                <CardProps 
+                  key={index}
+                  Title = {blog.Title}
+                  Date = {blog.Date}
+                  Url = {blog.Url}
+                  Company = {blog.Company}
+                />
               )}
             </div>
+          )}
           </div>
       </header>
     </div>
--- tech-search/src/components/Card.js
+++ tech-search/src/components/Card.js
@@ -1,19 +1,16 @@
-const Card = ({Title, Date, Url, Company}) => {
+import { Card } from 'semantic-ui-react';
+
+const CardProps = ({Title, Date, Url, Company}) => {
   const company_bucket = process.env.REACT_APP_BACKETENDPOINT + Company + process.env.REACT_APP_IMEXTENSION
   return (
-    <div className="card-item">
-      <div className="card-img">
-        <img alt={Company} src={company_bucket} />
-      </div>
-      <div className="card-title">
-        <a href={Url}><h3>{Title}</h3></a>
-      </div>
-      <div className="card-subInfo">
-        <div className="company">{Company}</div>
-        <div className="date">{Date}</div>
-      </div>
-    </div>
+    <Card
+      href={Url}
+      image={company_bucket}
+      header={Company}
+      meta={Date}
+      description={Title}
+    />
   )
 }
 
-export default Card
+export default CardProps
